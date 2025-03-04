7762128c2a0e3f9da9ffeb57e19e4bf86c467a24
johnchd <jcrynick@gmai.com>
2025-03-04 15:46:13 -0500

    re-dockerize-1.1

--- dotnet-app-3/docker.sh
+++ dotnet-app-3/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker rm -f dotnet-app-3
 docker build -t dotnet-app-3 .
-docker run -it --name dotnet-app-3 -p 5250:8080 dotnet-app-3 # cant reach BFF, can reach API
+docker run -it --name dotnet-app-3 -p 5250:8080 dotnet-app-3
 
 # Run locally:
 # APIROOT=http://localhost:5250 dotnet run
